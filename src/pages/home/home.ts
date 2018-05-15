6619140bc8d671b7d34e4cf88096ec92122ca8cb
dennis.menjivar <dennismenjivar@Denniss-MacBook-Pro.local>
2018-05-14 19:00:50 -0600

    Creada la parte de botones numericos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  principalButtons: ButtonCalculatorClass[];
+
   constructor(public navCtrl: NavController) {
+    this.loadButtons();
+  }
+
+  loadButtons() {
+    this.principalButtons = [];
+    let numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, -1, 0, -2];
+    for (let n of numbers) {
+      if (n == -1) {
+        this.principalButtons.push(new ButtonCalculatorClass(n, '', false));
+      } else if (n == -2) {
+        this.principalButtons.push(new ButtonCalculatorClass(n, 'AC', true));
+      } else {
+        this.principalButtons.push(new ButtonCalculatorClass(n, n.toString(), true));
+      }
+    }
 
   }
 
+}
+
+export class ButtonCalculatorClass {
+  id: number;
+  name: string;
+  enabled: boolean;
+
+  constructor(pID: number, pName: string, pEnabled: any) {
+    this.id = pID;
+    this.name = pName;
+    this.enabled = pEnabled;
+  }
 }
